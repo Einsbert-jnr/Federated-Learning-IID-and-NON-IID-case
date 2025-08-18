2842ae54459274715bbfbc49cbe33333de583af3
Einsbert <cheanampong@gmail.com>
2025-08-18 09:04:54 +0000

    Traidtional training

---
+++ FL Based Optimal Power Prediction-D2D/conventionalTraining.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from model_ann import get_ann
+from sklearn.utils import shuffle
+
+# Will carry out conventional or traditional training here
