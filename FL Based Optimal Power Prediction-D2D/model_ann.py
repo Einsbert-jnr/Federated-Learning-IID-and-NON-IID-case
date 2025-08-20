2fd12c241ae78db5e05e595aa883ff0b9194aeba
Einsbert <cheanampong@gmail.com>
2025-08-20 14:06:16 +0000

    model definition

--- FL Based Optimal Power Prediction-D2D/model_ann.py
+++ FL Based Optimal Power Prediction-D2D/model_ann.py
@@ -2,13 +2,16 @@ import tensorflow as tf
 
 def get_ann():
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Input(shape=(17,)))  # 15 features
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dropout(0.3))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Input(shape=(15,)))  # 15 features
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    # model.add(tf.keras.layers.BatchNormalization())
+    # model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    # model.add(tf.keras.layers.BatchNormalization())
+    # model.add(tf.keras.layers.Dropout(0.3))
     model.add(tf.keras.layers.Dense(1, activation='linear'))
     
     model.compile(
